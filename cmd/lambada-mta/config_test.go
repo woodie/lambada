ca6947608eda51ab21c7e6d641d6d4dfd5de23aa
woodie <woodie@netpress.com>
2026-07-21 01:39:51 -0700

    Add URL for veneer example.

--- cmd/lambada-mta/config_test.go
+++ cmd/lambada-mta/config_test.go
@@ -9,23 +9,6 @@ import (
 // Allow all tests in this package to use lowercase expect()
 func expect[T any](got T, t testing.TB) Expectation[T] { return Expect(got, t) }
 
-// Improve readability with vars set for structural functions and lifecycle hooks
-/*
-func TestCalculator(t *testing.T) {
-	spec.Run(t, "Calculator", func(t *testing.T, describe spec.G, it spec.S) {
-
-		context, before, after := describe, it.Before, it.After // HERE
-
-		var calculator *Calculator
-		before(func() { calculator = NewCalculator() })
-		after(func() { calculator.Clear() })
-
-		describe("#add", func() {
-			context("with positive numbers", func() {
-				it("returns the correct sum", func() {
-					expect(calculator.Add(2, 3), t).To(Equal(5))
-				})
-			})
-		})
-	})
-} */
+// Improve readability with structural functions and lifecycle hooks.
+// Right below spec.Run -> context, before, after := describe, it.Before, it.After
+// https://gist.github.com/woodie/35ee3fc2bea01b775b95b3fe5e950a05#file-example-go-L3
